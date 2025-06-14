cc6c2bdbd9d3816a61af1aeaf0de3da4478c1f29
Sagun Adhikari <sagunadhikari100@gmail.com>
2025-06-14 13:25:10 +0545

    feat(api): add public/protected API endpoints with JWT auth and initial user app
    Set up DRF views using ModelViewSet for public and protected endpoints. Integrated JWT authentication using SimpleJWT. Added initial user app for user-related models and views.

--- api/views.py
+++ api/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework.generics import ModelViewSet
+
+class UserModelViewSets(viewset.ModelViewSet):
+    qureyset = User.objects.all()
+    serializers = UserSerializer
 
 # Create your views here.
